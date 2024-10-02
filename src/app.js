ee8f57c74f11101ae26dfed5e62e0fb6b80a9846
Ines-sanz <inessanzm@hotmail.es>
2024-10-02 18:24:53 +0000

    FIrst commit

--- src/app.js
+++ src/app.js
@@ -2,9 +2,33 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
+  const who = [
+    "My goldfish ",
+    "An alien ",
+    "The pizza delivery guy ",
+    "My clone "
+  ];
+  const action = ["burnt ", "froze ", "stole ", "hid "];
+  const what = [
+    "the Wi-Fi router ",
+    "my keys ",
+    "the car ",
+    "the secret formula "
+  ];
+  const when = [
+    "when I wasn't looking",
+    "right before the deadline",
+    "while I was at the cinema",
+    "during my hollidays",
+    "during the zombie apocalypse"
+  ];
+
+  const getRandom = arra => Math.floor(Math.random() * arra.lenth);
+  const getValue = arra => arra[getRandom(arra)];
+
+  document.querySelector("#excuse").innerHTML = `${getValue(who)} ${getValue(
+    action
+  )} ${getValue(what)} ${getValue(when)}`;
 };
